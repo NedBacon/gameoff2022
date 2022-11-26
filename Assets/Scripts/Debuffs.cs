eeae30fdc8206c7c7f968ee894a4d67dc85a40e5
Ned Bacon <93846126+NedBacon@users.noreply.github.com>
2022-11-26 11:39:03 -0500

    More Debuffs
    Eyelid and color tint

--- Assets/Scripts/Debuffs.cs
+++ Assets/Scripts/Debuffs.cs
@@ -8,15 +8,15 @@ public class Debuffs : MonoBehaviour {
     public Slider slider;
     public int seconds;
     public List<string> debuffs;
+    public Image colorTint;
 
     private void Awake() {
         
-        debuffs = new List<string> {"button", "nothing", "button"};
+        debuffs = new List<string> {"button", "nothing", "button", "color"};
 
     }
 
     private void Update() {
-        Debug.Log(PanelGame.buttonsAmount);
 
         slider.value += Time.deltaTime / seconds;
 
@@ -27,12 +27,16 @@ public class Debuffs : MonoBehaviour {
             }
 
             int r = Random.Range(0, debuffs.Count);
-            if (debuffs[r] == "nothing") {
+            if(debuffs[r] == "nothing") {
                 Debug.Log("nothing");
             }
-            if (debuffs[r] == "button") {
+            if(debuffs[r] == "button") {
                 Debug.Log("button");
                 DebuffButton();
+            }
+            if(debuffs[r] == "color") {
+                Debug.Log("color");
+                DebuffColor();
             }
 
             slider.value = 0;
@@ -48,6 +52,23 @@ public class Debuffs : MonoBehaviour {
             debuffs.Remove("button");
             debuffs.Remove("button");
         }
+
+    }
+
+    void DebuffColor() {
+
+        switch(Random.Range(0, 3)) {
+            case 0 : 
+                colorTint.color = new Color(0.7f, 0, 0.7f, 0.4f); // purple
+            break;
+            case 1 : 
+                colorTint.color = new Color(0.7f, 0.7f, 0, 0.4f); // yellow
+            break;
+            case 2 : 
+                colorTint.color = new Color(0.7f, 0.35f, 0, 0.4f); // orange
+            break;
+        }
+
 
     }
 
